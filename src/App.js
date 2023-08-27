4f58e2a17c4e65419eda6af2a245eb38cdef7255
MohammadAmir1 <albertamir2002@gmail.com>
2023-08-28 00:03:31 +0530

    udpating env file

--- src/App.js
+++ src/App.js
@@ -3,15 +3,29 @@ import SearchIcon from "./search.svg";
 import "./App.css";
 import MovieCard from "./MovieCard";
 
-const API_URL = "http://www.omdbapi.com?apikey=b1fb52f7";
+const API_URL = process.env.REACT_APP_API_KEY;
 const App = () => {
   const [movies, setMovies] = useState([]);
 const[searchTerm, setSearchTerm]=useState('');
+
+
+
+
+console.log(movies,"movies")
 
   const searchMovies = async (title) => {
     const response = await fetch(`${API_URL}&s=${title}`);
     const data = await response.json();
     setMovies(data.Search);
+  };
+
+
+ const handleKeyPress = (event) => {
+    if (event.key === 'Enter') {
+      console.log('Enter key pressed');
+      searchMovies(searchTerm);
+      setSearchTerm("")
+    }
   };
 
   useEffect(() => {
@@ -21,13 +35,13 @@ const[searchTerm, setSearchTerm]=useState('');
   return (
     <div className="app">
       <h1>Nethub</h1>
-      <div className="search">
+      <div onKeyDown={handleKeyPress} className="search">
         <input
           placeholder="Search for  movies"
           value={searchTerm}
           onChange={(e) =>  setSearchTerm(e.target.value)}
         />
-        <img src={SearchIcon} alt="search" onClick={() =>{searchMovies(searchTerm);setSearchTerm([]) } }/>
+        {/* <img src={SearchIcon} alt="search" onClick={() =>{searchMovies(searchTerm);setSearchTerm('') } }/> */}
       </div>
 
       {movies?.length > 0 ? (
